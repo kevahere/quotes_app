b31ac2f367286d6c530d6163b3a8b8b889cadb69
kevahere <kevahere@gmail.com>
2018-08-10 16:49:54 +0300

    added goal-component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quotes = [
+
+    new Quote(1, "Misery loves company", "Le Miserable"),
+    new Quote(2, "You can sleep when you die", "Taheed Epps"),
+    new Quote(3, "Simba mwenda pole ndiye mla nyama", "Snoop Lion"),
+  ]
 
   constructor() { }
 
