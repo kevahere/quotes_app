772a0ced3d623215c9e710a03439c524cfddc928
kevahere <kevahere@gmail.com>
2018-08-10 16:18:41 +0300

    created project and quote class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { Quote } from './quote'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'quotes';
-}
+
+    quotes = [
+      new Quote(1,"Misery loves company","Le Miserable"),
+      new Quote(2,"You can sleep when you die","Taheed Epps"),
+      new Quote(3,"Simba mwenda pole ndiye mla nyama","Snoop Lion"),
+    ];
+  }
